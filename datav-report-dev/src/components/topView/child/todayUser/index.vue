7f94ba0fd8ad7f9409f03e80ff7507722fba771e
jiajing <jiajing@zhiweidata.com>
2021-02-17 00:19:35 +0800

    今日交易用户数完成:echarts柱状图

--- datav-report-dev/src/components/topView/child/todayUser/index.vue
+++ datav-report-dev/src/components/topView/child/todayUser/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <common-card />
+  <common-card title="今日交易用户数" value="81,014">
+    <template>
+      <div id="today_users_chart" :style="{ width: '100%', height: '100%' }"></div>
+    </template>
+    <template v-slot:footer>
+      <span>退货率</span>
+      <span class="emphasis">5.14%</span>
+    </template>
+  </common-card>
 </template>
 
 <script>
@@ -7,6 +15,49 @@ import commonCardMixin from "@/components/mixins/commonCardMixin.js";
 export default {
   name: "TodayUser",
   mixins: [commonCardMixin],
+  mounted() {
+    const chartDom = document.getElementById("today_users_chart");
+    const chart = this.$echarts.init(chartDom);
+    chart.setOption({
+      color:'#3398DB',
+      xAxis: {
+        type: "category",
+        boundaryGap: false,
+        data: [
+          "00:00",
+          "01:00",
+          "02:00",
+          "03:00",
+          "04:00",
+          "05:00",
+          "06:00",
+          "07:00",
+          "08:00",
+          "09:00",
+          "10:00",
+          "11:00",
+          "12:00",
+          "13:00",
+        ],
+      },
+      yAxis: {
+        show: false,
+      },
+      grid: {
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+      },
+      series: [
+        {
+          type: "bar",
+          barWidth:'60%',
+          data: [410, 82, 200, 334, 390, 330, 220, 150, 82, 200, 134, 290, 330, 150],
+        },
+      ],
+    });
+  },
 };
 </script>
 

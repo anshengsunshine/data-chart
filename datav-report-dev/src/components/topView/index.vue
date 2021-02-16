af0919c1d3e73d4e1374b57b403013649407ca7f
jiajing <jiajing@zhiweidata.com>
2021-02-16 18:09:27 +0800

    引入element-ui:card/row/col,完成顶部核心指标组件开发

--- datav-report-dev/src/components/topView/index.vue
+++ datav-report-dev/src/components/topView/index.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="top_view">
-     <el-card shadow="hover">
-      鼠标悬浮时显示
-    </el-card>
-     <el-card shadow="hover">
-      鼠标悬浮时显示
-    </el-card>
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <el-card shadow="hover"> 鼠标悬浮时显示 </el-card>
+      </el-col>
+     <el-col :span="6">
+        <el-card shadow="hover"> 鼠标悬浮时显示 </el-card>
+      </el-col>
+     <el-col :span="6">
+        <el-card shadow="hover"> 鼠标悬浮时显示 </el-card>
+      </el-col>
+    <el-col :span="6">
+        <el-card shadow="hover"> 鼠标悬浮时显示 </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
-name:"TopView"
-}
+  name: "TopView",
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- datav-report-dev/src/plugins/element.js
+++ datav-report-dev/src/plugins/element.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
-import { Button, Card } from 'element-ui'
+import { Button, Card, Row, Col } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)

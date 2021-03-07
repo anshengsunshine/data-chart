f855bd53c30dc81fe7d1c0e398391dfb8d9bcf37
jiajing <jiajing@zhiweidata.com>
2021-03-07 23:38:26 +0800

    引入v-chart;

--- datav-report-dev/src/components/salesView/index.vue
+++ datav-report-dev/src/components/salesView/index.vue
@@ -1,13 +1,45 @@
 <template>
-  <div>SalesView</div>
+  <!-- <v-chart :options="data" /> -->
+  <ve-line :data="data" />
 </template>
 
 <script>
 export default {
   name: "SalesView",
+  data() {
+    return {
+      // data: {
+      //   xAxis: {
+      //     type: "category",
+      //   },
+      //   yAxis: {},
+      //   series: [
+      //     {
+      //       type: "line",
+      //       data: [100, 200, 400, 120, 300],
+      //     },
+      //   ],
+      // },
+      data: {
+        colums: ["日期", "销售额"],
+        rows: [
+          { 日期: "1月1日", 销售额: 123 },
+          { 日期: "1月2日", 销售额: 1123 },
+          { 日期: "1月3日", 销售额: 1223 },
+          { 日期: "1月4日", 销售额: 1233 },
+          { 日期: "1月5日", 销售额: 11123 },
+          { 日期: "1月6日", 销售额: 12223 },
+          { 日期: "1月7日", 销售额: 12333 },
+        ],
+      },
+    };
+  },
 };
 </script>
 
 <style scoped>
-
+.echarts {
+  width: 100%;
+  height: 100%;
+}
 </style>
--- datav-report-dev/src/main.js
+++ datav-report-dev/src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
-import * as Echarts from 'echarts';
+import * as echarts from 'echarts'
+// import * as echarts from 'echarts/lib/echarts'
 import "@/assets/style/main.scss";
+import VueEcharts from "vue-echarts";
+import './plugins/element.js'
+import './plugins/vcharts.js'
 
 Vue.config.productionTip = false
-Vue.prototype.$echarts = Echarts
+Vue.prototype.$echarts = echarts
+Vue.component('v-chart',VueEcharts)
 
 new Vue({
   router,

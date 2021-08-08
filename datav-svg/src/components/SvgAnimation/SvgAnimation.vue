6aa007b5f269e0e122988e5aaff5ab5b9f83a7e5
anshengsunshine <646143658@qq.com>
2021-08-09 00:38:29 +0800

    新增svg动画文件,暂无内容

---
+++ datav-svg/src/components/SvgAnimation/SvgAnimation.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {
+  name: "SvgAnimation",
+};
+</script>
+<style type="text/css">
+.container{
+  width: 200px;
+  height: 200px;
+  border: 1px solid #333;
+}
+</style>
--- datav-svg/src/components/index.js
+++ datav-svg/src/components/index.js
@@ -1,5 +1,7 @@
-import Icon from "@/components/Icon/index"
+import Icon from "@/components/Icon/index";
+import SvgAnimation from "@/components/SvgAnimation/index";
 
 export default function (Vue) {
     Vue.use(Icon)
+    Vue.use(SvgAnimation)
 }

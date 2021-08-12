d8847d81446556e7430781ae62ae3b591ba57139
anshengsunshine <646143658@qq.com>
2021-08-12 13:06:35 +0800

    svg 图标引入

--- datav-svg/src/components/Test1/Test1Comp.vue
+++ datav-svg/src/components/Test1/Test1Comp.vue
@@ -34,7 +34,24 @@
         <polyline points="20 10, 80 50, 20 90" fill="currentColor"></polyline>
       </symbol>
       <symbol id="arrowRight" viewBox="0 0 100 100">
-        <polyline points="20 10, 80 50, 20 90" fill="transparent" stroke="currentColor" stroke-width="3"></polyline>
+        <polyline
+          points="20 10, 80 50, 20 90"
+          fill="transparent"
+          stroke="currentColor"
+          stroke-width="3"
+        ></polyline>
+      </symbol>
+      <symbol id="comments" viewBox="0 0 1024 1024">
+        <path
+          fill="currentColor"
+          d="M896.2 175.8H128.7c-8.8 0-16 7.2-16 16V923c0 6.7 4.1 12.6 10.4 15 1.8 0.7 3.7 1 5.6 1 4.5 0 9-1.9 12.1-5.5l186.5-213.6H564c8.8 0 16-7.2 16-16s-7.2-16-16-16H320c-4.6 0-9 2-12.1 5.5l-163.3 187V207.8h735.5v480.1H771.5c-8.8 0-16 7.2-16 16s7.2 16 16 16h124.7c8.8 0 16-7.2 16-16V191.8c0-8.9-7.2-16-16-16z"
+          p-id="1286"
+        ></path>
+        <path
+          fill="currentColor"
+          d="M665.5 687.9h-50c-8.8 0-16 7.2-16 16s7.2 16 16 16h50c8.8 0 16-7.2 16-16s-7.2-16-16-16zM256 432.2c-8.8 0-16 7.2-16 16s7.2 16 16 16h125.1c8.8 0 16-7.2 16-16s-7.2-16-16-16H256zM574.3 432.2H449.2c-8.8 0-16 7.2-16 16s7.2 16 16 16h125.1c8.8 0 16-7.2 16-16s-7.2-16-16-16zM767.9 464.2c8.8 0 16-7.2 16-16s-7.2-16-16-16H642.8c-8.8 0-16 7.2-16 16s7.2 16 16 16h125.1z"
+          p-id="1287"
+        ></path>
       </symbol>
     </defs>
   </svg>
--- datav-svg/src/views/Icon.vue
+++ datav-svg/src/views/Icon.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <icon prefix="" name="comments"  :style="{ width: '50px', height: '50px', color: '#f00' }"></icon>
     <icon
       prefix="icon-"
       name="code"
@@ -9,7 +10,7 @@
     <icon
       prefix="icon-"
       name="all"
-      :style="{ width: '100px', height: '100px', color: '#f00' }"
+      :style="{ width: '100px', height: '100px', color: 'green' }"
     ></icon>
     <icon
       prefix=""

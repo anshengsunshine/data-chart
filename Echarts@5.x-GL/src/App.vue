eb24214eb6380000b421d786b731d9ffa48d5245
anshengsunshine <646143658@qq.com>
2021-09-26 10:38:30 +0800

    echarts-gl：3D地球、3D散点图开发

---
+++ Echarts@5.x-GL/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- 3D 点图 -->
+  <gL-scatter-3D />
+  <!-- 3D 地图 -->
+  <!-- <gL-earth-3D /> -->
+</template>
+
+<script>
+// import GLEarth3D from "./echartsGLEarth3D";
+import GLScatter3D from "./echartsGLScatter3D";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  components: {
+    // GLEarth3D,
+    GLScatter3D
+  },
+});
+</script>
+
+<style scoped>
+.chart {
+  height: 100vh;
+}
+</style>
+
+<style>
+body {
+  margin: 0;
+}
+</style>

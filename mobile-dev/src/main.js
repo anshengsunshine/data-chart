1ffa7ecff1c6a2cd14819f31a79fbae91678825f
anshengsunshine <646143658@qq.com>
2021-10-27 08:05:26 +0800

    引入echarts/vue-echarts，绘制bar图

--- mobile-dev/src/main.js
+++ mobile-dev/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./assets/style/reset.css"
+import "./assets/style/reset.css";
+import VChart from 'vue-echarts';
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App)
+app.use(store).use(router).mount("#app");
+app.component('v-chart', VChart)
--- mobile-dev/src/views/Home.vue
+++ mobile-dev/src/views/Home.vue
@@ -2,16 +2,49 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <div class="test_text">测试测试rem</div>
+    <v-chart class="chart" autoresize :option="barOption" />
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+import { use } from "echarts/core";
+import { BarChart } from "echarts/charts";
+import { onMounted } from "@vue/runtime-core";
+use([BarChart]);
 export default {
   name: "Home",
+  setup() {
+    const barOption = ref({});
+    onMounted(() => {
+      barOption.value.value = {
+        xAxis: {
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        },
+        yAxis: {
+          type: "value",
+        },
+        series: [
+          {
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: "bar",
+          },
+        ],
+      };
+    });
+    return {
+      barOption,
+    };
+  },
 };
 </script>
 <style lang="less" scoped>
-.test_text{
+.test_text {
   font-size: 32px;
+}
+.chart {
+  width: 300px;
+  height: 500px;
 }
 </style>
